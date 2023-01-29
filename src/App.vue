0b1f4971b68cc025f7fff2f8da056df65a3ba8ac
Julian Ardila <julian.ardila@uptc.edu.co>
2023-01-29 18:27:13 -0500

    Added SplashScreen for load data

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,33 @@
 <script setup lang="ts">
-import MyHome from '@/components/MyHome.vue';
+import SplashScreen from '@/components/SplashScreen.vue'
+import { defineAsyncComponent } from 'vue'
+
+const Home = defineAsyncComponent(() =>
+  import('@/components/MyHome.vue')
+)
 </script>
 
 <template>
-  <MyHome />
+  <Suspense>
+    <template #default>
+      <Home />
+    </template>
+    <template #fallback>
+      <SplashScreen />
+    </template>
+  </Suspense>
 </template>
 
-<style scoped>
+<style>
+html,
+body,
+.app {
+  min-height: 100vh;
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+* {
+  --brand-green: #04b500;
+  --brand-blue: #0689b0;
+}
 </style>
